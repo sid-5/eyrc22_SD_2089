d7ac0133763bc8bd611248805dc8881a92c003a1
ath2212 <puranik.atharva36@gmail.com>
2022-12-08 11:09:57 +0530

    Georef added

--- sentinel_drone/sentinel_drone/scripts/block_detection.py
+++ sentinel_drone/sentinel_drone/scripts/block_detection.py
@@ -91,6 +91,33 @@ class Edrone():
 		else:
 			return False
 
+	def geoReference(self, image):
+		"""
+		image -> captured by drone.
+		"""
+		img1 = cv2.imread('task2d.tif')  # CHECK THE PATH to the map.
+		img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+		img2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+		#sift
+		sift = cv2.SIFT_create()
+
+		keypoints_1, descriptors_1 = sift.detectAndCompute(img1,None)
+		keypoints_2, descriptors_2 = sift.detectAndCompute(img2,None)
+
+		#feature matching
+		bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+		matches = bf.match(descriptors_1,descriptors_2)
+		matches = sorted(matches, key = lambda x:x.distance)
+
+		for m in matches[:50]:
+			x_base, y_base = (keypoints_1[m.queryIdx].pt)
+			x_img, y_img = (keypoints_2[m.trainIdx].pt)
+			x_new, y_new = self.pixel2coord(x_base, y_base)
+
+			#EXECUTE THE QUERIES FOR GEOREF -> THEN OPEN THE GEOTIFF FILE -> PIXEL2COORD on the centre point wrt translated geotiff file.
+
 	def get_image(self,img_msg):
 		br = bridge()
 		img = br.imgmsg_to_cv2(img_msg, "passthrough")
